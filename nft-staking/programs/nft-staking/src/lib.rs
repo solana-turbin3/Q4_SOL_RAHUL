eb656be348770bd90d0b7c401c106b7d94fe6b79
Rahul <rahulmishrapu22@gmail.com>
2024-11-05 18:05:28 +0530

    nft-staking

---
+++ nft-staking/programs/nft-staking/src/lib.rs
@@ -0,0 +1,34 @@
+use anchor_lang::prelude::*;
+
+declare_id!("3j9cqQvy43a3VNyh51Pun9Tavy4XCX3LGMp6C4usaTfD");
+
+mod state;
+mod instructions;
+mod error;
+
+pub use instructions::*;
+
+#[program]
+pub mod nft_staking {
+    use super::*;
+
+    pub fn initialize_config(ctx: Context<InitializeConfig>, points_per_stake: u8, max_stake: u8, freeze_period: u32) -> Result<()> {
+        ctx.accounts.initialize_config(points_per_stake, max_stake, freeze_period, &ctx.bumps)
+    }
+
+    pub fn initialize_user(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.initialize_user(&ctx.bumps)
+    }
+
+    pub fn stake(ctx: Context<Stake>) -> Result<()> {
+        ctx.accounts.stake(&ctx.bumps)
+    }
+
+    pub fn unstake(ctx: Context<Unstake>) -> Result<()> {
+        ctx.accounts.unstake()
+    }
+
+    pub fn claim(ctx: Context<Claim>) -> Result<()> {
+        ctx.accounts.claim()
+    }
+}
